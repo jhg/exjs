2f3c1777f4d539b7ffe9779c74a769c545ed353f
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2017-05-21 15:14:27 +0100

    Refactor of function name

--- lib/exjs.ex
+++ lib/exjs.ex
@@ -11,98 +11,98 @@ defmodule Exjs do
 
   ## Examples
 
-    iex> Exjs.compile_from_string "x"
+    iex> Exjs.compile_from_string! "x"
     "x"
 
-    iex> Exjs.compile_from_string "0"
+    iex> Exjs.compile_from_string! "0"
     "0"
 
-    iex> Exjs.compile_from_string "2.0"
+    iex> Exjs.compile_from_string! "2.0"
     "2.0"
 
-    iex> Exjs.compile_from_string "5 / 2"
+    iex> Exjs.compile_from_string! "5 / 2"
     "2.5"
 
-    iex> Exjs.compile_from_string "4 / 2"
+    iex> Exjs.compile_from_string! "4 / 2"
     "2.0"
 
-    iex> Exjs.compile_from_string "x / 2"
+    iex> Exjs.compile_from_string! "x / 2"
     "x/2"
 
-    iex> Exjs.compile_from_string "x = 2"
+    iex> Exjs.compile_from_string! "x = 2"
     "x=2"
 
-    iex> Exjs.compile_from_string "x = y / 2"
+    iex> Exjs.compile_from_string! "x = y / 2"
     "x=y/2"
 
-    iex> Exjs.compile_from_string "x = nil"
+    iex> Exjs.compile_from_string! "x = nil"
     "x=null"
 
-    iex> Exjs.compile_from_string "x = [1, 2, 3, 4]"
+    iex> Exjs.compile_from_string! "x = [1, 2, 3, 4]"
     "x=[1,2,3,4]"
 
-    iex> Exjs.compile_from_string "x = {1, 2, 3, 4}"
+    iex> Exjs.compile_from_string! "x = {1, 2, 3, 4}"
     "x=[1,2,3,4]"
 
-    iex> Exjs.compile_from_string "x = [1, 2]"
+    iex> Exjs.compile_from_string! "x = [1, 2]"
     "x=[1,2]"
 
-    iex> Exjs.compile_from_string "x = {1, 2}"
+    iex> Exjs.compile_from_string! "x = {1, 2}"
     "x=[1,2]"
 
-    iex> Exjs.compile_from_string "1 + 2 + 3 + 4 + 5 + 6 + 7 + 8"
+    iex> Exjs.compile_from_string! "1 + 2 + 3 + 4 + 5 + 6 + 7 + 8"
     "36"
 
-    iex> Exjs.compile_from_string "1 * 2 * 3 * 4 * 5 * 6 * 7 * 8"
+    iex> Exjs.compile_from_string! "1 * 2 * 3 * 4 * 5 * 6 * 7 * 8"
     "40320"
 
-    iex> Exjs.compile_from_string "(1 + 2) * 3"
+    iex> Exjs.compile_from_string! "(1 + 2) * 3"
     "9"
 
-    iex> Exjs.compile_from_string "1 + 2 * 3"
+    iex> Exjs.compile_from_string! "1 + 2 * 3"
     "7"
 
-    iex> Exjs.compile_from_string "Window.Console.log []"
+    iex> Exjs.compile_from_string! "Window.Console.log []"
     "window.console.log([])"
 
-    iex> Exjs.compile_from_string "List.first [1, 2]"
+    iex> Exjs.compile_from_string! "List.first [1, 2]"
     "List.first([1,2])"
 
-    iex> Exjs.compile_from_string "List.first x"
+    iex> Exjs.compile_from_string! "List.first x"
     "List.first(x)"
 
-    iex> Exjs.compile_from_string "length x"
+    iex> Exjs.compile_from_string! "length x"
     "x.length"
 
-    iex> Exjs.compile_from_string "fn(x) -> x end"
+    iex> Exjs.compile_from_string! "fn(x) -> x end"
     "function(x){return x}"
 
-    iex> Exjs.compile_from_string "fn(x) -> 4 + x end"
+    iex> Exjs.compile_from_string! "fn(x) -> 4 + x end"
     "function(x){return 4+x}"
 
-    iex> Exjs.compile_from_string "fn(x) -> 4 * x end"
+    iex> Exjs.compile_from_string! "fn(x) -> 4 * x end"
     "function(x){return 4*x}"
 
-    iex> Exjs.compile_from_string "fn(x) -> 4 - x end"
+    iex> Exjs.compile_from_string! "fn(x) -> 4 - x end"
     "function(x){return 4-x}"
 
-    iex> Exjs.compile_from_string "fn(x) -> 4 / x end"
+    iex> Exjs.compile_from_string! "fn(x) -> 4 / x end"
     "function(x){return 4/x}"
 
-    iex> Exjs.compile_from_string "fn(x) -> length(x) end"
+    iex> Exjs.compile_from_string! "fn(x) -> length(x) end"
     "function(x){return x.length}"
 
-    iex> Exjs.compile_from_string "def x do\\n  0\\nend"
+    iex> Exjs.compile_from_string! "def x do\\n  0\\nend"
     "this.x=function(){return 0}"
 
-    iex> Exjs.compile_from_string "def x do\\n  x = 0\\n  x\\nend"
+    iex> Exjs.compile_from_string! "def x do\\n  x = 0\\n  x\\nend"
     "this.x=function(){x=0;return x}"
 
-    iex> Exjs.compile_from_string "defp same(x), do: x"
+    iex> Exjs.compile_from_string! "defp same(x), do: x"
     "function same(x){return x}"
 
   """
-  def compile_from_string(source_code) do
+  def compile_from_string!(source_code) do
     source_code
     |> Parser.parse!
     |> Optimizer.optimize
@@ -112,6 +112,6 @@ defmodule Exjs do
   def compile_from_file!(filename) do
     filename
     |> File.read!
-    |> compile_from_string
+    |> compile_from_string!
   end
 end
